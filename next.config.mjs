055b33f4f9bccc6de25119ea0549538244fa46d3
Igor <igorushakov111@gmail.com>
2025-02-09 21:23:22 +0700

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 const nextConfig = {
-  trailingSlash: true, // Добавляет слэш в конце URL
-  distDir: "out", // Необязательно, но может помочь при экспорте
-  eslint: {
-    ignoreDuringBuilds: true, // Отключает ESLint во время сборки, если нужно
-  },
+  output: "export", // Включает режим статической генерации
 };
 
 export default nextConfig;
